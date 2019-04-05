3005b0994968785d7141edd3371bccc3ae9155e0
Behnam Mohammadi <itten@live.com>
2019-04-05 23:35:40 +0430

    added minify and removed comments from build code

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 const presets = [
-    [
-      '@babel/env',
-      {
-        targets: '>0.2%, not dead, not ie <= 11, not op_mini all'
-      }
-    ]
-  ];
-  
-  module.exports = { presets };
-  
+  [
+    '@babel/env',
+    {
+      targets: '>0.2%, not dead, not ie <= 11, not op_mini all'
+    }
+  ],
+  ['minify']
+];
+
+module.exports = { presets, comments: false };
